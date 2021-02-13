42f01b5a62d458a98fde36cc833451ea553360fd
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-02-13 17:11:22 -0600

    Shifted folder up one level

---
+++ adventium-proto/src/components/Parameters/Parameters.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Parameters = () => (
+    <div>
+        <textarea />
+        <div>
+            <input type="text" />
+            <button type="button">+</button>
+        </div>
+    </div>
+);
+
+export default Parameters;
--- ui-prototype/adventium-proto/src/components/Parameters/Parameters.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Parameters = () => <div>Parameters</div>;
-
-export default Parameters;
