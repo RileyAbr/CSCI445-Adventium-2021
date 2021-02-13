426ef4782d32038453c28db9078d3af206de0f92
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-02-13 16:57:20 -0600

    Added UI prototype project files

---
+++ ui-prototype/adventium-proto/src/components/Assertions/Assertions.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Assertions = () => <div>Assertions</div>;
+
+export default Assertions;
---
+++ ui-prototype/adventium-proto/src/components/Output/Output.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Output = () => <div>Output</div>;
+
+export default Output;
