4175c012889c02945caf655f186ca6410ca1bc66
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-02-14 12:07:14 -0600

    Finished styling for Parameters screen

--- ui-prototype/src/components/Parameters/Parameters.js
+++ ui-prototype/src/components/Parameters/Parameters.js
@@ -32,17 +32,41 @@ const alphaSet = [
 const Parameters = () => (
     <>
         <h2>Parameters</h2>
-        <div>
-            <select size="8">
-                {alphaSet.map((val) => (
-                    <option value={val}>Sample parameter {val}</option>
-                ))}
-            </select>
-            <button type="button">-</button>
-        </div>
-        <div>
-            <input type="text" />
-            <button type="button">+</button>
+        <div
+            style={{
+                display: "flex",
+                height: "50%",
+                flexFlow: "column nowrap",
+                justifyContent: "space-between",
+                alignItems: "center",
+            }}
+        >
+            <div
+                style={{
+                    width: "50%",
+                    display: "flex",
+                    flexFow: "row nowrap",
+                    alignItems: "center",
+                }}
+            >
+                <select style={{ flexGrow: 1 }} size="8">
+                    {alphaSet.map((val) => (
+                        <option value={val}>Sample parameter {val}</option>
+                    ))}
+                </select>
+                <button type="button">-</button>
+            </div>
+            <div
+                style={{
+                    width: "50%",
+                    display: "flex",
+                    flexFow: "row nowrap",
+                    alignItems: "center",
+                }}
+            >
+                <input type="text" style={{ flexGrow: 1 }} />
+                <button type="button">+</button>
+            </div>
         </div>
     </>
 );
