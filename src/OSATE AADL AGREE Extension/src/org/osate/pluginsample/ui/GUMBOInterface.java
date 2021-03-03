28db028b020fa87835b260682048dc1398d74270
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-03-02 23:45:50 -0600

    Put together a single page of the Plugin UI

--- src/OSATE AADL AGREE Extension/src/org/osate/pluginsample/ui/GUMBOInterface.java
+++ src/OSATE AADL AGREE Extension/src/org/osate/pluginsample/ui/GUMBOInterface.java
@@ -5,20 +5,6 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class GUMBOInterface extends JFrame {
-//	public static void createGUMBOWindow() {
-//		JFrame frame = new JFrame("AGREE Creator");
-//		frame.setSize(750, 600);
-//		frame.setLocationRelativeTo(null);
-//		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//		frame.setVisible(true);
-//		
-//        frame.setLayout(new FlowLayout());
-//        
-//        frame.add(labelQuestion);
-//        frame.add(labelWeight);
-//        frame.add(fieldWeight);
-//        frame.add(buttonTellMe);
-//	}
 	
 	public GUMBOInterface() {
 	    super("AGREE Creator");
@@ -32,7 +18,71 @@ public class GUMBOInterface extends JFrame {
 	}
 	
 	private void initComponents() {
-		setLayout(new FlowLayout());
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.PAGE_AXIS));
+		
+//		Header Panel
+		JPanel headerPanel = new JPanel();
+		headerPanel.setLayout(new FlowLayout());
+		
+		JLabel headerLabel = new JLabel("Assumptions");
+		headerLabel.setFont(new java.awt.Font("Arial", Font.PLAIN, 32));
+		headerPanel.add(headerLabel);
+		
+		contentPanel.add(headerPanel);
+		
+//		List Panel
+		JPanel listPanel = new JPanel();
+		listPanel.setLayout(new FlowLayout());
+		
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+        for (int i = 0; i < 4; i++) {
+        	listModel.addElement(
+        			String.format("assume \"Sample assumption\" : (%s < %d)", AGREEComponentFactory.createParameter(), 300)
+					);
+		}
+        JList<String> countryList = new JList<>(listModel); 
+        listPanel.add(countryList);
+        
+        JButton removeAssumptionButton = new JButton("-");
+        listPanel.add(removeAssumptionButton);
+        
+		contentPanel.add(listPanel);
+		
+//		Inputs Panel
+        JPanel inputsPanel = new JPanel();
+        inputsPanel.setLayout(new BoxLayout(inputsPanel, BoxLayout.PAGE_AXIS));
+        
+        JTextField agreeDescriptionTextField = new JTextField();
+        inputsPanel.add(agreeDescriptionTextField);
+        
+        JComboBox<String> assumptionOperandList = new JComboBox<>(AGREEComponentFactory.getAllParameters());
+        inputsPanel.add(assumptionOperandList);
+        
+        JComboBox<String> assumptionComparatorList = new JComboBox<>(AGREEComponentFactory.getAllAssumptionComparators());
+        inputsPanel.add(assumptionComparatorList);
+        
+        JTextField assumptionValueTextField = new JTextField();
+        inputsPanel.add(assumptionValueTextField);
+        
+        JButton addAssumptionButton = new JButton("+");
+        inputsPanel.add(addAssumptionButton);
+        
+        contentPanel.add(inputsPanel);
+        
+//		Pagination Panel
+        JPanel paginationPanel = new JPanel();
+        paginationPanel.setLayout(new FlowLayout());
+        
+        JButton backButton = new JButton("Back");
+        paginationPanel.add(backButton);
+        
+        JButton nextButton = new JButton("Next");
+        paginationPanel.add(nextButton);
+        
+        contentPanel.add(paginationPanel);
+        
+        add(contentPanel);     
 	}
 	
 	public static void main(String[] args) {
